5467414b9e22df8399123299c481587bc5499828
PrplYoru <edoardomorosanu05@gmail.com>
2024-04-09 19:07:46 +0200

    Added JWT token

--- src/handlers/auth.rs
+++ src/handlers/auth.rs
@@ -2,8 +2,21 @@ use actix_web::{web, HttpResponse, Responder};
 use actix_web::web::Data;
 use mysql::prelude::Queryable;
 use mysql::{params, Pool};
-use mysql::serde_json::json;
-use crate::models::auth::{UserInfo, LoginInfo};
+use mysql::serde_json::{json, Map};
+use crate::models::auth::{UserInfo, LoginInfo, TokenInfo};
+use jsonwebtoken::{encode, Header, EncodingKey, Algorithm};
+use jsonwebtoken::errors::Result as JWTResult;
+
+fn generate_token(user_id: i32, email: &str, user_type: &str) -> JWTResult<String> {
+    let claims = TokenInfo {
+        user_id,
+        email: email.to_string(),
+        user_type: user_type.to_string(),
+    };
+
+    let secret = b"@vrs%|fumAjH_N|r}d/W(@/KD!/0F*&),d0$26_R-*gKb=PJF(d,j'wcT&we^[]";
+    encode(&Header::new(Algorithm::HS256), &claims, &EncodingKey::from_secret(secret))
+}
 
 pub async fn register_user(info: web::Json<UserInfo>, pool: Data<Pool>) -> impl Responder {
     let mut conn = match pool.get_conn() {
@@ -47,19 +60,27 @@ pub async fn login_user(info: web::Json<LoginInfo>, pool: Data<Pool>) -> impl Re
         Err(_) => return HttpResponse::InternalServerError().json(json!({"message": "Errore di connessione al database"})),
     };
 
-    let select_stmt = r"SELECT password, verified FROM users WHERE email = :email";
-    let result: Option<(String, bool)> = match conn.exec_first(select_stmt, params! {"email" => &info.email}) {
+    let select_stmt = r"SELECT id, password, tipo_utente, verified FROM users WHERE email = :email";
+    let result: Option<(i32, String, String, bool)> = match conn.exec_first(select_stmt, params! {"email" => &info.email}) {
         Ok(result) => result,
         Err(_) => return HttpResponse::InternalServerError().json(json!({"message": "Errore nell'esecuzione della query"})),
     };
 
     match result {
-        Some((stored_password, verified)) => {
+        Some((user_id, stored_password, user_type, verified)) => {
             if bcrypt::verify(&info.password, &stored_password).unwrap() {
                 if verified {
-                    HttpResponse::Ok().json(json!({"message": "Login effettuato con successo", "verified": true}))
+                    let token = match generate_token(user_id, &info.email, &user_type) {
+                        Ok(token) => token,
+                        Err(_) => return HttpResponse::InternalServerError().json(json!({"message": "Errore nella generazione del token"})),
+                    };
+                    HttpResponse::Ok().json(json!({"message": "Login effettuato con successo", "verified": true, "token": token}))
                 } else {
-                    HttpResponse::Ok().json(json!({"message": "Login effettuato con successo", "verified": false}))
+                    let token = match generate_token(user_id, &info.email, &user_type) {
+                        Ok(token) => token,
+                        Err(_) => return HttpResponse::InternalServerError().json(json!({"message": "Errore nella generazione del token"})),
+                    };
+                    HttpResponse::Ok().json(json!({"message": "Login effettuato con successo", "verified": false, "token": token}))
                 }
             } else {
                 HttpResponse::BadRequest().json(json!({"message": "Password errata"}))
--- src/models/auth.rs
+++ src/models/auth.rs
@@ -1,4 +1,4 @@
-use serde::Deserialize;
+use serde::{Serialize, Deserialize};
 
 #[derive(Deserialize)]
 pub struct UserInfo {
@@ -11,4 +11,11 @@ pub struct UserInfo {
 pub struct LoginInfo {
     pub email: String,
     pub password: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct TokenInfo {
+    pub user_id: i32,
+    pub email: String,
+    pub user_type: String,
 }
